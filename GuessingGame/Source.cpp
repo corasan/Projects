089413eb901c1a80d196e4e0eb16e85bce4fbe5b
Henry Paulino <henrypl360@gmail.com>
2014-03-16 00:56:53 -0400

    2

---
+++ GuessingGame/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+	int guess; 
+	srand(time(0));
+	int random = rand() % 20;
+	int tries = 0;
+
+	cout << "Guess a number between 1-20, you have 3 tries: ";
+	cin >> guess;
+	
+	while (guess != random && tries < 3) {
+		if (guess < random) {
+			cout << "Number is higher. Guess again: ";
+			cin >> guess;
+		}
+		if (guess > random) {
+			cout << "Number is lower. Guess again: ";
+			cin >> guess;
+		}
+		tries++;
+	}
+
+	if (guess == random){
+		cout << "Correct! " << endl;
+	}
+	else {
+		cout << "The number was " << random << endl;
+		cin >> guess;
+	}
+	system("pause");
+	return 0;
+}
